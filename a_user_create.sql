694a9a8b599def2fd4b49fab8dd70de9ad566e52
PJ <140643635+DarthCerberus@users.noreply.github.com>
2025-04-24 14:51:15 +0530

    Sync with setup - one time run

--- a_user_create.sql
+++ a_user_create.sql
@@ -1,9 +1,9 @@
 -- Create the user
-CREATE USER C##test IDENTIFIED BY "password";
+CREATE USER C##testing IDENTIFIED BY "password";
 
 -- Grant privileges to the user so it can connect and access resources.
-GRANT CREATE SESSION TO C##test;
-GRANT UNLIMITED TABLESPACE TO C##test;
+GRANT CREATE SESSION TO C##testing;
+GRANT UNLIMITED TABLESPACE TO C##testing;
 -- Or grant specific tablespace quotas as needed.
-GRANT CREATE TABLE, CREATE SEQUENCE, CREATE PROCEDURE TO C##test; 
-GRANT CREATE TRIGGER TO C##test;
+GRANT CREATE TABLE, CREATE SEQUENCE, CREATE PROCEDURE TO C##testing; 
+GRANT CREATE TRIGGER TO C##testing;
