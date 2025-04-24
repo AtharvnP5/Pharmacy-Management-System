ceca7098921c8858bb763c39148d98d6de3cf5bb
PJ <140643635+DarthCerberus@users.noreply.github.com>
2025-04-24 13:38:33 +0530

    small fix

--- test_p3.sql
+++ test_p3.sql
@@ -9,7 +9,6 @@ DBMS_OUTPUT.PUT_LINE('--- Testing non-existent patient 999 on 19-APR-2025 ---');
   Get_Prescription_Details(999, TO_DATE('19-04-2025', 'DD-MM-YYYY'));
 
 DBMS_OUTPUT.PUT_LINE('--- Testing valid patient with potentially problematic date (string) ---');
-BEGIN
   Get_Prescription_Details(102, 'INVALID_DATE');
 EXCEPTION
   WHEN OTHERS THEN
